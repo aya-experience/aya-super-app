e0b63ed23fe1812b86a7237d1b868b834267a7ab
Yasmina Schoueri <yasmina.schoueri.ext@eulerhermes.com>
2018-08-24 16:27:17 +0200

    styling

--- aya-xp-expertise/src/components/Navbar.js
+++ aya-xp-expertise/src/components/Navbar.js
@@ -3,13 +3,8 @@ import React from "react";
 export const Navbar = () => (
   <nav className="navbar is-fixed-top" aria-label="main navigation">
     <div className="navbar-brand flex flex-column items-center">
-      <a
-        className="icon-kanji dib self-center white f2 pa3"
-        href="http://aya-experience.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-      </a>
+        <a className="icon-kanji dib self-center white f2 pa3" href="http://aya-experience.com"
+            target="_blank" rel="noopener noreferrer" />
         <div className="oval"/>
     </div>
   </nav>
