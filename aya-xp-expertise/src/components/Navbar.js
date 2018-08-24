9383f16649a3c2c0c094a6311dce29d990655a39
mfrachet <marvin.frachet@zenika.com>
2018-08-24 14:15:26 +0200

    wip

--- aya-xp-expertise/src/components/Navbar.js
+++ aya-xp-expertise/src/components/Navbar.js
@@ -5,11 +5,18 @@ import computer from "../computer_animated.svg";
 export const Navbar = () => (
   <nav className="navbar is-fixed-top" aria-label="main navigation">
     <div className="navbar-brand flex flex-row items-center">
-        <a className="navbar-item dib w-10 self-center" href="http://aya-experience.com" target="_blank" rel="noopener noreferrer">
-            <img src={kanji} alt="Aya experience" height="30" />
-        </a>
-        <div className="ttu tc flex-auto self-center">Team</div>
-        <div className="self-center pt2 pr2"><img src={computer} alt="Aya experience" width="30" /></div>
+      <a
+        className="navbar-item dib self-center"
+        href="http://aya-experience.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={kanji} alt="Aya experience" height="20" width="20" />
+      </a>
+      <div className="ttu tc flex-auto self-center">Team</div>
+      <div className="self-center pt2 pr2">
+        <img src={computer} alt="Aya experience" width="30" />
+      </div>
     </div>
   </nav>
 );
