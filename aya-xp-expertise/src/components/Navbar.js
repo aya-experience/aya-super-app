bf1b2008bbfb7eaa04699860342f07519bfb5322
mfrachet <marvin.frachet@zenika.com>
2018-08-24 16:45:55 +0200

    wip

--- aya-xp-expertise/src/components/Navbar.js
+++ aya-xp-expertise/src/components/Navbar.js
@@ -1,11 +1,16 @@
+/* eslint-disable */
 import React from "react";
 
 export const Navbar = () => (
   <nav className="navbar is-fixed-top" aria-label="main navigation">
     <div className="navbar-brand flex flex-column items-center">
-        <a className="icon-kanji dib self-center white f2 pa3" href="http://aya-experience.com"
-            target="_blank" rel="noopener noreferrer" />
-        <div className="oval"/>
+      <a
+        className="icon-kanji dib self-center white f2 pa3"
+        href="http://aya-experience.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      />
+      <div className="oval" />
     </div>
   </nav>
 );
