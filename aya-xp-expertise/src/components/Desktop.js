5bf9e971d3d8355866bd4368255a831cb2376087
mfrachet <marvin.frachet@zenika.com>
2018-08-24 17:31:16 +0200

    desktop page

--- aya-xp-expertise/src/components/Desktop.js
+++ aya-xp-expertise/src/components/Desktop.js
@@ -1,7 +1,9 @@
 import React from "react";
 import io from "socket.io-client";
 import { Notification } from "./Notification";
-import qr from "./qr.png";
+import qr from "./qrwhite.png";
+import mobile from "./mobile.svg";
+import arrow from "./arrow.svg";
 
 export class Desktop extends React.Component {
   constructor(props) {
@@ -18,19 +20,41 @@ export class Desktop extends React.Component {
       this.setState({ isMobileAvailable: true });
 
       setTimeout(() => {
-        window.location.replace("http://stackoverflow.com");
+        window.location.replace("http://192.168.1.134:3000/team");
       }, 3000);
     });
   }
 
   render() {
     return (
-      <div className="bg-near-black app">
-        <img src={qr} alt="qr code" className="qr" />
-        <Notification
-          text="A decide has been successfully connected. Wait a second for being redirected"
-          isVisible={this.state.isMobileAvailable}
-        />
+      <div className="bg-near-black app desktop">
+        <div className="container">
+          <div className="mega-size">
+            <div className="icon-kanji" />
+            <h1>TEAM</h1>
+          </div>
+
+          <div className="columns weird-margin">
+            <div className="column">
+              <p className="p-mobile custom-margin">
+                Visite nos planètes directement...
+              </p>
+              <img src={arrow} alt="arrow" className="arrow" />
+            </div>
+            <div className="column">
+              <img src={mobile} alt="mobile" className="mobile-mini" />
+              <p className="p-mobile">
+                OU prend le controle avec ton smartphone
+              </p>
+              <img src={qr} alt="qr" className="qr" />
+            </div>
+          </div>
+
+          <Notification
+            text="A decide has been successfully connected. Wait a second for being redirected"
+            isVisible={this.state.isMobileAvailable}
+          />
+        </div>
       </div>
     );
   }
